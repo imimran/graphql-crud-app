2565d52c9000accaceccc85a389af17ce8f4c6b4
imimran <engr.aih@gmail.com>
2023-10-10 23:48:54 +0600

    Category crud apis

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,5 +1,24 @@
 export const typeDefs = `#graphql
+type Category {
+    id: ID!
+    name: String!
+    parent: Category
+    isActive: Boolean!
+  }
+  
+  input CategoryInput {
+    name: String!
+    parent: ID
+  }
+  
   type Query {
-    hello: String
+    getAllCategories: [Category]
+    getCategory(id: ID!): Category
+  }
+  
+  type Mutation {
+    createCategory(input: CategoryInput!): Category
+    updateCategory(id: ID!, input: CategoryInput!): Category
+    deleteCategory(id: ID!): Boolean
   }
   `;
