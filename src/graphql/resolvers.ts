ea91d289d98c9ef330937c9bcd99aa29c0b527d1
imimran <engr.aih@gmail.com>
2023-10-11 08:50:28 +0600

    Deactive category with child

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -22,6 +22,32 @@ const resolvers = {
       await Category.findByIdAndDelete(id);
       return true;
     },
+    deactivateCategory: async (_: any, { id }: { id: string }) => {
+      // Find the category to delete
+      const category = await Category.findById(id);
+      if (!category) {
+        throw new Error('Category not found');
+      }
+
+      // Deactivate the category and save it
+      category.isActive = false;
+      await category.save();
+
+      // Recursively deactivate child categories
+      async function deactivateChildCategories(parentId: string) {
+        const childCategories = await Category.find({ parent: parentId });
+        for (const childCategory of childCategories) {
+          childCategory.isActive = false;
+          await childCategory.save();
+          await deactivateChildCategories(childCategory._id);
+        }
+      }
+
+      // Start deactivating child categories recursively
+      await deactivateChildCategories(id);
+
+      return true;
+    },
   },
   Category: {
     parent: async (category: any) => {
